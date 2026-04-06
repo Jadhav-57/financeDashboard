3ab67fce533c748430937219fb8e6c2b4d2ad28c
Aditya Jadhav <aditya3160@gmail.com>
2026-04-06 20:48:46 +0530

    walha

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,8 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { RecoilRoot } from "recoil";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
     <App />
-  </StrictMode>,
 
 )
